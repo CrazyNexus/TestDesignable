d60c8b7753f1ce8ec71b16796f609c563109d06b
Thomas Dubiel <td@crazynexus.de>
2018-01-25 23:02:47 +0100

    Initial Commit

---
+++ TestDesignable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestDesignable
+//
+//  Created by Thomas Dubiel on 25.01.18.
+//  Copyright © 2018 Thomas Dubiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
